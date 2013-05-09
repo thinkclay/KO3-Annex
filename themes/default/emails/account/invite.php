0a04859337776e57b1154ae336f4332386da710a
root <root@ve.g8rd5dm7.vesrv.com>
2013-05-09 00:39:40 -0700

    tons of minor tweaks

--- themes/default/emails/account/invite.php
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-  <title>Invitation to P2Bi</title>
-</head>
-<body>
-  <p>Someone has recommended P2Bi. Start the process now to become our next <?php echo $type; ?>!</p>
-  <?php echo HTML::anchor(url::site('/account/register'), 'Complete Registration'); ?>
-</body>
-</html>
